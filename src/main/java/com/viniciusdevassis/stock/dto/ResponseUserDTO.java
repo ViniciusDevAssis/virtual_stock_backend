f651b1d84e56e08d81380a8072e4f2a00b2a0655
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2025-03-01 23:44:57 -0300

    Criação da entidade User e seus DTOs

---
+++ src/main/java/com/viniciusdevassis/stock/dto/ResponseUserDTO.java
@@ -0,0 +1,37 @@
+package com.viniciusdevassis.stock.dto;
+
+public class ResponseUserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+
+    public ResponseUserDTO() {
+    }
+
+    public ResponseUserDTO(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/viniciusdevassis/stock/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package com.viniciusdevassis.stock.mapper;
+
+import com.viniciusdevassis.stock.dto.CreateUserDTO;
+import com.viniciusdevassis.stock.dto.ResponseUserDTO;
+import com.viniciusdevassis.stock.dto.UpdateUserDTO;
+import com.viniciusdevassis.stock.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    CreateUserDTO userToCreateUserDTO(User user);
+    User createUserDTOToUser(CreateUserDTO dto);
+    ResponseUserDTO userToUserResponseDTO(User user);
+    User responseUserDTOToUser(ResponseUserDTO dto);
+    UpdateUserDTO userToUpdateUserDTO(User user);
+    User updateUserDTOToUser(UpdateUserDTO dto);
+}
